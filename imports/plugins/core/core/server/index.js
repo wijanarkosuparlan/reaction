7f56fea680534b794235b935852d55c3647f1790
Peter Lyons <pete@reactioncommerce.com>
2019-03-15 14:00:02 -0600

    chore(logging): Log uncaught exceptions
    Signed-off-by: Peter Lyons <pete@reactioncommerce.com>

--- imports/plugins/core/core/server/index.js
+++ imports/plugins/core/core/server/index.js
@@ -8,7 +8,10 @@ import startup from "./startup";
 // handle any unhandled Promise rejections because
 // Node 8 no longer swallows them
 process.on("unhandledRejection", (err) => {
-  Logger.error(err);
+  Logger.error(err, "unhandledRejection logged by reaction core");
+});
+process.on("uncaughtException", (err) => {
+  Logger.error(err, "uncaughtException logged by reaction core");
 });
 
 Meteor.methods(methods);
